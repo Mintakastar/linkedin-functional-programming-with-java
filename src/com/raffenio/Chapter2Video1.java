327806c63274c3c4f375c96d75279a62aa349223
Raffenio <mintaka.star@gmail.com>
2024-08-03 00:57:38 -0700

    First Class function Chapter 2 Video 1

---
+++ src/com/raffenio/Chapter2Video1.java
@@ -0,0 +1,35 @@
+package com.raffenio;
+
+import java.util.function.Function;
+
+public class Chapter2Video1 {
+
+    protected static class MyMath{
+        public static Integer triple(Integer x){
+            return x*3;
+        }
+    }
+    public static void main(String[] args) {
+        //        input     output
+        Function<Integer, Integer> myTripple = MyMath::triple;
+
+        Integer result = myTripple.apply(5);
+        System.out.println(result);
+
+        //a class method
+        //cualquier metodo de una clase lo podemos pasar a una funcion
+
+
+        System.out.println("\nEjemplo ABS de math con una funcion (todos saldran positivos)");
+
+        Function<Integer,Integer> myabs = Math::abs;   // el metodo estatico abc, lo pasamos a una variable del tipo de la interfaz funcion
+
+        System.out.println(myabs.apply(-10));
+        System.out.println(myabs.apply(-20));
+        System.out.println(myabs.apply(-30));
+        System.out.println(myabs.apply(40));
+
+
+    }
+}
+
