907c0e81b152a02d583a58e2f029ad5d7fc7be1e
Raffenio <mintaka.star@gmail.com>
2024-08-05 19:05:25 -0700

    applying streams challenge

---
+++ src/com/raffenio/Chapter3Challenge_appliyingStreams.java
@@ -0,0 +1,52 @@
+package com.raffenio;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Chapter3Challenge_appliyingStreams {
+
+
+    // Return the largest number in the 'numbers' array
+    static List<String> filterAndTransform(List<Integer> numbers) {
+        List<String> numbersWords =
+                numbers.stream()
+                        .filter(num->num%3==0)
+                        .map(
+                                (numb)->{
+
+                                    switch (numb){
+                                        case 1: return "one";
+                                        case 2: return "two";
+                                        case 3: return "three";
+                                        case 4: return "four";
+                                        case 5: return "five";
+                                        case 6: return "six";
+                                        case 7: return "seven";
+                                        case 8: return "eigth";
+                                        case 9: return "nine";
+                                        case 0: return "zero";
+                                        default: return "?";
+                                    }
+
+                                }).toList();
+
+        // Your code goes here
+        return numbersWords;
+    }
+    public static void main(String[] args) {
+
+// This is how your code will be called.
+// Your answer should be the largest value in the numbers array.
+// You can edit this code to try different testing cases.
+        Integer[] numbersArr = { 1, 5, 3, 6, 8, 9, 2 };
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(numbersArr));
+        List<String> result = Chapter3Challenge_appliyingStreams.filterAndTransform(numbers);
+
+        System.out.println(numbers);
+        System.out.println(result);
+
+
+    }
+}
